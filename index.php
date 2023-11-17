80e6288b019df949fcfc07ab7bdb3075b3c57724
Ivan <ivanmarsanazorin@gmail.com>
2023-11-17 01:19:27 +0100

    Estilos

--- index.php
+++ index.php
@@ -7,18 +7,88 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ShopSwap</title>
+
+    <link rel="stylesheet" href="estilo.css">
+
     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
     <link rel="manifest" href="images/site.webmanifest">
+    <style>
+
+    </style>
 </head>
 <body>
     <header>
-        <h1></h1>
-        <nav></nav>
+        <nav id="navbar">
+        <ul class="navbar-items flexbox-col">
+            <li class="navbar-logo flexbox-left">
+            <a class="navbar-item-inner flexbox">
+                <img src="images/favicon-32x32.png" alt="Imagen">
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="search-outline"></ion-icon>
+                </div>
+                <span class="link-text">Search</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="home-outline"></ion-icon>
+                </div>
+                <span class="link-text">Home</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="folder-open-outline"></ion-icon>
+                </div>
+                <span class="link-text">Projects</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="pie-chart-outline"></ion-icon>
+                </div>
+                <span class="link-text">Dashboard</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="people-outline"></ion-icon>
+                </div>
+                <span class="link-text">Team</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="chatbubbles-outline"></ion-icon>
+                </div>
+                <span class="link-text">Support</span>
+            </a>
+            </li>
+            <li class="navbar-item flexbox-left">
+            <a class="navbar-item-inner flexbox-left">
+                <div class="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="settings-outline"></ion-icon>
+                </div>
+                <span class="link-text">Settings</span>
+            </a>
+            </li>
+        </ul>
+        </nav>
     </header>
-    <main>
-        
+    <main id="main" class="flexbox-col">
+        <h2>Lorem ipsum!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis, rerum doloremque iste sed voluptates omnis molestias molestiae animi recusandae labore sit amet delectus ad necessitatibus laudantium qui! Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?</p>
     </main>
     <footer>
 
