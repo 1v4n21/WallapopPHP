fde6fa5890a520c731adc44a5c9410f82c5efd7a
Ivan <ivanmarsanazorin@gmail.com>
2023-11-18 19:22:42 +0100

    8

--- registro.php
+++ registro.php
@@ -104,6 +104,8 @@
                     </div>
                     <div>
                         <div class="flex -mx-3">
+
+                            <!-- Nombre -->
                             <div class="w-1/2 px-3 mb-5">
                                 <label for="" class="text-xs font-semibold px-1">Nombre</label>
                                 <div class="flex">
@@ -111,6 +113,8 @@
                                     <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Jose">
                                 </div>
                             </div>
+
+                            <!-- Numero de telefono -->
                             <div class="w-1/2 px-3 mb-5">
                                 <label for="" class="text-xs font-semibold px-1">Numero de Telefono</label>
                                 <div class="flex">
@@ -119,6 +123,8 @@
                                 </div>
                             </div>
                         </div>
+
+                        <!-- Poblacion -->
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-5">
                                 <label for="" class="text-xs font-semibold px-1">Poblacion</label>
@@ -128,6 +134,8 @@
                                 </div>
                             </div>
                         </div>
+
+                        <!-- Email -->
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-5">
                                 <label for="" class="text-xs font-semibold px-1">Email</label>
@@ -137,6 +145,8 @@
                                 </div>
                             </div>
                         </div>
+
+                        <!-- Contraseña1 -->
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-12">
                                 <label for="" class="text-xs font-semibold px-1">Contraseña</label>
@@ -158,7 +168,7 @@
                             </div>
                         </div>
 
-                        <!-- Contraseña2 -->
+                        <!-- Boton de formulario -->
                         <div class="flex -mx-3">
                             <div class="w-full px-3 mb-5">
                                 <button class="block w-full max-w-xs mx-auto bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTRATE AHORA</button>
