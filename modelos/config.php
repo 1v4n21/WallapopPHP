a7d1e3ffdac1e04d152c674cb3881e81006789e9
TH3M1S <131470445+1v4n21@users.noreply.github.com>
2024-01-17 16:38:37 +0100

    Update config.php

--- modelos/config.php
+++ modelos/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Variables de configuracion para la conexion a la DB
-define('MYSQL_USER', 'root');
-define('MYSQL_PASS', '');
-define('MYSQL_DB', 'ShopSwap');
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', getenv('DB_USERNAME'));
+define('MYSQL_PASS', getenv('DB_PASSWORD'));
+define('MYSQL_DB', getenv('DB_NAME'));
+define('MYSQL_HOST', getenv('DB_HOST'));
